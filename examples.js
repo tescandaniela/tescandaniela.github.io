b7e8b56f878571177ff225e3c8af8cdd3c3742be
Tescan Daniela <tescandaniela6@gmail.com>
2023-11-20 20:42:17 +0200

    extract js in new file

---
+++ examples.js
@@ -0,0 +1,19 @@
+console.info(2 + 3);
+console.warn("daniela");
+
+var employed = true;
+var age = 18;
+console.log("angajat", employed);
+employed = false;
+console.log("angajat", employed);
+employed = "da";
+console.log("angajat", employed);
+
+var Skills = ["html", "css"];
+Skills;
+
+console.debug("tipul variabilei skills:", typeof skills);
+
+var job = document.getElementById("job-title");
+console.warn("job", job);
+console.info(typeof job);
