c0f9d991bf520c5b0613fb261cb68b8cdbaa49f3
Tescan Daniela <tescandaniela6@gmail.com>
2023-11-27 21:52:51 +0200

    did the 4 pgs thing

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 function ShowHome() {
-  var page = (getElementById("HOME").style.display = "none");
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("Skills").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+
+  var page = document.getElementById("HOME");
   page.style.display = "block";
+}
 
-  function ShowProjects() {
-    var oldPage = document.getElementById("projects");
-    oldPage.style.display = "block";
-    var page = document.getElementById("languages");
-    page.style.display = "none";
-  }
-  function ShowLanguages() {
-    var page = document.getElementById("languages");
-    page.style.display = "block";
-  }
-  function ShowSkills() {
-    var page = document.getElementById("Skills");
-    page.style.display = "none";
-  }
+function ShowProjects() {
+  document.getElementById("languages").style.display = "none";
+  document.getElementById("HOME").style.display = "none";
+  document.getElementById("Skills").style.display = "none";
 
-  var oldPage2 = document.getElementById("projects");
-  oldPage.style.display = "none";
+  var page = document.getElementById("projects");
+  page.style.display = "block";
+}
+function ShowLanguages() {
+  document.getElementById("HOME").style.display = "none";
+  document.getElementById("Skills").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+
+  var page = document.getElementById("languages");
+  page.style.display = "block";
+}
+function ShowSkills() {
+  document.getElementById("HOME").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+
+  var page = document.getElementById("Skills");
+  page.style.display = "block";
 }
