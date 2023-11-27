4fcd4ffebc679c33ffd8f56989ea1ab1708042cf
Tescan Daniela <tescandaniela6@gmail.com>
2023-11-27 21:41:33 +0200

    amuitat

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-function ShowPojects() {
-  var oldPage = document.getElementById("projects");
-  oldPage.style.display = "block";
-}
-function ShowLanguages() {
-  var page = document.getElementById("languages");
-  page.style.display = "block";
-}
-function ShowSkills() {
-  var page = document.getElementById("Skills");
-  page.style.display = "block";
-}
 function ShowHome() {
-  var page = getElementById("HOME");
+  var page = (getElementById("HOME").style.display = "none");
   page.style.display = "block";
+
+  function ShowProjects() {
+    var oldPage = document.getElementById("projects");
+    oldPage.style.display = "block";
+    var page = document.getElementById("languages");
+    page.style.display = "none";
+  }
+  function ShowLanguages() {
+    var page = document.getElementById("languages");
+    page.style.display = "block";
+  }
+  function ShowSkills() {
+    var page = document.getElementById("Skills");
+    page.style.display = "none";
+  }
+
+  var oldPage2 = document.getElementById("projects");
+  oldPage.style.display = "none";
 }
