7df0c5c5280b661359ebaa3360f6d05782edf556
Tescan Daniela <tescandaniela6@gmail.com>
2024-01-22 21:33:19 +0200

    sort by endorcements or by name

--- index.js
+++ index.js
@@ -42,7 +42,18 @@ function initEvents() {
   });
 }
 
+function sortSkillsByEndorcements(a, b) {
+  console.info("sort", a, b);
+  return b.endorcements - a.endorcements;
+}
+
+function sortByName(a, b) {
+  return a.name.localeCompare(b.name);
+}
+
 function showSkills(skills) {
+  //skills.sort(sortSkillsByEndorcements)
+  skills.sort(sortSkillsByEndorcements);
   var ul = $("#skills ul");
 
   var text = skills.map(function (skill) {
