c2d3076bb870c7ccded626321361977f1f7aa6b6
Tescan Daniela <tescandaniela6@gmail.com>
2024-01-29 21:17:49 +0200

    used arrow fs

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function showPage(id) {
 
 function initEvents() {
   const toolbar = $("#top-menu-bar");
-  toolbar.addEventListener("click", function (e) {
+  toolbar.addEventListener("click", (e) => {
     if (e.target.matches("a")) {
       const page = e.target.dataset.page;
       console.warn("click %o", page);
@@ -58,7 +58,7 @@ function showSkills(skills) {
   skills.sort(sortSkillsByEndorcements);
   const ul = $("#skills ul");
 
-  const text = skills.map(function (skill) {
+  const text = skills.map((skill) => {
     let cls = "";
     if (skill.favorite === true) {
       cls = "favorite";
@@ -74,8 +74,8 @@ function showSkills(skills) {
 }
 
 function loadSkills() {
-  fetch("skills.json").then(function (r) {
-    r.json().then(function (skills) {
+  fetch("skills.json").then((r) => {
+    r.json().then((skills) => {
       showSkills(skills);
     });
   });
